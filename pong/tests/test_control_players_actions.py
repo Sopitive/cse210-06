a79128c8ba328bcfbfda93212bea8e2cd4bf45ab
macewhit <macymwhitney@gmail.com>
2022-02-12 00:09:00 -0700

    added tests for cast and actions

--- pong/tests/test_control_players_actions.py
+++ pong/tests/test_control_players_actions.py
@@ -11,6 +11,8 @@ from game.scripting.script import Script
 from game.casting.cast import Cast
 from game.casting.paddle import Paddle
 from game.scripting.action import Action
+from game.services.keyboard_service import KeyboardService
+from game.casting.actor import Actor
 
 
 class CustomKeyboardService:
@@ -34,6 +36,11 @@ def test_control_player1_actions():
     paddle = action.get_paddle(cast)
     assert paddle.get_velocity().equals(Point(0, -constants.PADDLE_SPEED))
 
+    action = ControlPlayer1Action(CustomKeyboardService("s"))
+    action.execute(cast, None)
+    paddle = action.get_paddle(cast)
+    assert paddle.get_velocity().equals(Point(0, constants.PADDLE_SPEED))
+
 def test_action():
     """This test asserts the execute function of the Action class that raises a NotImplementedError"""
     cast = Cast()
@@ -51,4 +58,24 @@ def test_script():
     script.remove_action("action", action)
     assert len(script.get_actions("action")) == 0
 
+def test_control_actors_action():
+    "tests the functions within the ControlActorsAction class"
+    keyboard_service = KeyboardService()
+    cast = Cast()
+    control_actors_action = ControlActorsAction(keyboard_service)
+    with pytest.raises(NotImplementedError):
+        control_actors_action.is_control_down('w')
+    with pytest.raises(NotImplementedError):
+        control_actors_action.get_paddle(cast)
 
+def test_cast_actors():
+    """ tests the methods of getting all actors, getting first actor and removing actor within the Cast class"""
+    cast = Cast()
+    actor = Actor()
+    actor.set_text("TEST")
+    cast.add_actor("group", actor)
+    first_actor = cast.get_first_actor("group")
+    assert first_actor.get_text() == "TEST"
+    assert len(cast.get_all_actors()) == 1
+    cast.remove_actor("group", actor)
+    assert len(cast.get_all_actors()) == 0
